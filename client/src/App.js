ceea72b2c970ba1c8d6b6f0919eba6612c1f1bdc
Georgia <ghobs987@gmail.com>
2022-11-08 10:55:19 +0000

    front end functionality for getting data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-function ApplianceList() {
-  return null
-}
+import { useState } from 'react'
+import Navtabs from './components/navigation/Navtabs'
+import Appliances from './components/maintabs/Appliances'
+import Settopbox from './components/maintabs/Settopbox'
+import Lighbulb from './components/maintabs/Lightbulb'
+import Smokedetector from './components/maintabs/Smokedetector'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Appliances')
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Settopbox') {
+      return <Settopbox />
+    }
+    if (currentPage === 'Lightbulb') {
+      return <Lighbulb />
+    }
+    if (currentPage === 'Smokedetector') {
+      // cv is returned through navtabs
+      return <Smokedetector />
+    }
+    return <Appliances />
+  }
+
+  const handlePageChange = (page) => setCurrentPage(page)
   return (
     <div className="App">
-      <h1>Appliances</h1>
-      <ApplianceList></ApplianceList>
+      <Navtabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
   )
 }
