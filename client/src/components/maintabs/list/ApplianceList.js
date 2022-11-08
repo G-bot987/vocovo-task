69072fefc12d50d3fcab7709a460edc29b1f5dbc
Georgia <ghobs987@gmail.com>
2022-11-08 12:47:40 +0000

    catching and logging for delete

--- client/src/components/maintabs/list/ApplianceList.js
+++ client/src/components/maintabs/list/ApplianceList.js
@@ -1,15 +1,22 @@
-// import { useEffect, useState } from 'react'
 import React from 'react'
 import styles from '../../../index.css'
 
 export default function ApplianceList(appliance) {
   const thisApp = appliance.appliance
   const { id, name, type } = thisApp
-
   function runDeleteId() {
     console.log('delete')
 
-    fetch(`/id/${id}`, { method: 'DELETE' }).then((res) => res.json())
+    fetch(`/id/${id}`, { method: 'DELETE' })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('could not delete')
+        }
+        console.log('success')
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   }
 
   return (
